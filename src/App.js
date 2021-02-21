1a19bc87789005fbf7c5d5369d6143f1297c6a57
Adarsh <adarsh2858@gmail.com>
2021-02-21 20:57:45 +0530

    add postgres and define a API to fetch the data for react app in the frontend

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 const data = [
@@ -41,19 +42,57 @@ const columns = [
 ];
 
 function App() {
+  const [merchants, setMerchants] = useState(false);
+
+  useEffect(() => {
+    getMerchant();
+  }, []);
+
+  function getMerchant() {
+    fetch("http://localhost:3001/")
+      .then((response) => {
+        return response.text();
+      })
+      .then((data) => {
+        setMerchants(data);
+      });
+  }
+
+  function createMerchant() {
+    let name = prompt("Enter the merchant's name");
+    let phone = prompt("Enter the merchant's phone");
+
+    fetch("http://localhost:3001/merchants", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ name, phone }),
+    })
+      .then((response) => {
+        return response.text();
+      })
+      .then((data) => {
+        alert(data);
+        getMerchant();
+      });
+  }
+
   return (
     <div className="App">
-      <header>
-        Personal Wallet
-      </header>
-        <div className="">
-          <a href="/all-wallets">All Wallets</a>
-          <a href="/new-wallet">New Wallet</a>
-          <a href="/check-balance">Check Balance</a>
-          <a href="/add-fund">Add Funds</a>
-          <a href="/spend-fund">Spend Funds</a>
-          <a href="/all-transactions">All Transactions</a>
-        </div>
+      <header>Personal Wallet</header>
+      {merchants ? merchants : "There is no merchant data available"}
+      <br />
+      <button onClick={createMerchant}>Add Merchant</button>
+
+      <div className="grid grid-row">
+        <a href="/all-wallets">All Wallets</a>
+        <a href="/new-wallet">New Wallet</a>
+        <a href="/check-balance">Check Balance</a>
+        <a href="/add-fund">Add Funds</a>
+        <a href="/spend-fund">Spend Funds</a>
+        <a href="/all-transactions">All Transactions</a>
+      </div>
 
       <table style={{ background: "#fff" }} className="">
         <thead>
---
+++ src/models/merchant.js
@@ -0,0 +1,58 @@
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+  user: "my_user",
+  host: "localhost",
+  database: "my_database",
+  password: "root",
+  port: 5432,
+});
+
+const getMerchants = () => {
+  return new Promise(function (resolve, reject) {
+    pool.query(
+      'SELECT * FROM personal_wallet ORDER BY user_id ASC',
+      (error, results) => {
+        if (error) reject(error);
+        resolve(results.rows);
+      }
+    );
+  });
+};
+
+const createMerchant = (body) => {
+  return new Promise(function (resolve, reject) {
+    const { name, phone } = body;
+
+    pool.query(
+      "INSERT INTO personal_wallet (username, phone) VALUES ($1, $2) RETURNING *",
+      [name, phone],
+      (error, results) => {
+        if (error) reject(error);
+        if (results)
+          resolve(`A new merchant has been added added: ${results.rows[0]}`);
+      }
+    );
+  });
+};
+
+const deleteMerchant = () => {
+  return new Promise(function (resolve, reject) {
+    const id = parseInt(request.params.id);
+
+    pool.query(
+      `DELETE FROM personal_wallet WHERE id = $1`,
+      [id],
+      (error, results) => {
+        if (error) reject(error);
+        resolve(`Merchant deleted with ID: ${id}`);
+      }
+    );
+  });
+};
+
+module.exports = {
+  getMerchants,
+  createMerchant,
+  deleteMerchant,
+};
