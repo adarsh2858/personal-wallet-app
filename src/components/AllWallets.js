093c954903be3ad6ae1593b81c25675b98db4637
adarsh2858 <adarsh2858@gmail.com>
2021-03-31 00:29:22 +0530

    fix the child and parent component returning JSX elements and make the new wallet form look as per design

--- src/components/AllWallets.js
+++ src/components/AllWallets.js
@@ -1,166 +1,50 @@
-import { useEffect, useState } from "react";
-
-const AllWallets = ({ allWallets, setAllWallets, newWallet, setNewWallet }) => {
-  const [merchants, setMerchants] = useState(false);
-
-  useEffect(() => {
-    getMerchant();
-  }, []);
-
-  function getMerchant() {
-    fetch("http://localhost:3001/")
-      .then((response) => {
-        return response.text();
-      })
-      .then((data) => {
-        setMerchants(data);
-      });
-  }
-
-  function createMerchant() {
-    let name = prompt("Enter the merchant's name");
-    let phone = prompt("Enter the merchant's phone");
-
-    fetch("http://localhost:3001/merchants", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify({ name, phone }),
-    })
-      .then((response) => {
-        return response.text();
-      })
-      .then((data) => {
-        alert(data);
-        getMerchant();
-      });
-  }
-
-  function deleteMerchant() {
-    let id = prompt("Enter the merchant's ID:");
-
-    fetch(`http://localhost:3001/merchant/${id}`, {
-      method: "DELETE",
-    })
-      .then((response) => {
-        return response.text();
-      })
-      .then((data) => {
-        alert(data);
-        getMerchant();
-      });
-  }
-  return (
-    <>
-      <div className="mt-4">
-        {merchants.length > 0
-          ? merchants
-          : "There is no merchant data available"}
-      </div>
-      <br />
-      <button
-        className="bg-green-400 p-4 m-4 rounded-sm"
-        onClick={createMerchant}
-      >
-        Add Merchant
-      </button>
-
-      <button
-        className="bg-red-400 p-4 m-4 rounded-sm"
-        onClick={deleteMerchant}
-      >
-        Delete Merchant
-      </button>
-      <div className="mx-4">
-        <header className="bg-blue-400 p-2 w-2/3">Personal Wallet UI</header>
-
-        <div className="grid grid-row float-left">
-          <button
-            className="bg-yellow-400 p-1 hover:bg-yellow-500"
-            onClick={() => {
-              setNewWallet(false);
-              setAllWallets(true);
-            }}
-          >
-            All Wallets
-          </button>
-          <button
-            className=" bg-blue-400 p-1  hover:bg-blue-500"
-            onClick={() => {
-              setAllWallets(false);
-              setNewWallet(true);
-            }}
-          >
-            New Wallet
-          </button>
-          <a
-            className=" bg-blue-400 p-1  hover:bg-blue-500"
-            href="/check-balance"
-          >
-            Check Balance
-          </a>
-          <a className=" bg-blue-400 p-1  hover:bg-blue-500" href="/add-fund">
-            Add Funds
-          </a>
-          <a className=" bg-blue-400 p-1  hover:bg-blue-500" href="/spend-fund">
-            Spend Funds
-          </a>
-          <a
-            className=" bg-blue-400 p-1  hover:bg-blue-500"
-            href="/all-transactions"
-          >
-            All Transactions
-          </a>
-        </div>
-      </div>
-
-      {allWallets ? (
-        <table className="w-1/2" style={{ background: "#fff" }}>
-          <thead>
-            <tr className=" bg-blue-200">
-              <th className="py-1 px-2">User Id</th>
-              <th className="px-2">Name</th>
-              <th className="px-2">Phone</th>
-              <th className="px-2">Balance (Rs)</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Usr1</td>
-              <td>Name 1</td>
-              <td>9988999877</td>
-              <td>765.43</td>
-            </tr>
-            <tr>
-              <td>Usr1</td>
-              <td>Name 1</td>
-              <td>9988999877</td>
-              <td>765.43</td>
-            </tr>
-            <tr>
-              <td>Usr1</td>
-              <td>Name 1</td>
-              <td>9988999877</td>
-              <td>765.43</td>
-            </tr>
-            <tr>
-              <td>Usr1</td>
-              <td>Name 1</td>
-              <td>9988999877</td>
-              <td>765.43</td>
-            </tr>
-            <tr>
-              <td>Usr1</td>
-              <td>Name 1</td>
-              <td>9988999877</td>
-              <td>765.43</td>
-            </tr>
-          </tbody>
-        </table>
-      ) : null}
-    </>
-  );
-};
+const AllWallets = ({ allWallets, setAllWallets, newWallet, setNewWallet }) => (
+  <>
+    {allWallets ? (
+      <table className="w-1/2" style={{ background: "#fff" }}>
+        <thead>
+          <tr className=" bg-blue-200">
+            <th className="py-1 px-2">User Id</th>
+            <th className="px-2">Name</th>
+            <th className="px-2">Phone</th>
+            <th className="px-2">Balance (Rs)</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Usr1</td>
+            <td>Name 1</td>
+            <td>9988999877</td>
+            <td>765.43</td>
+          </tr>
+          <tr>
+            <td>Usr1</td>
+            <td>Name 1</td>
+            <td>9988999877</td>
+            <td>765.43</td>
+          </tr>
+          <tr>
+            <td>Usr1</td>
+            <td>Name 1</td>
+            <td>9988999877</td>
+            <td>765.43</td>
+          </tr>
+          <tr>
+            <td>Usr1</td>
+            <td>Name 1</td>
+            <td>9988999877</td>
+            <td>765.43</td>
+          </tr>
+          <tr>
+            <td>Usr1</td>
+            <td>Name 1</td>
+            <td>9988999877</td>
+            <td>765.43</td>
+          </tr>
+        </tbody>
+      </table>
+    ) : null}
+  </>
+);
 
 export default AllWallets;
--- src/components/NewWallet.js
+++ src/components/NewWallet.js
@@ -1,34 +1,38 @@
 const NewWallet = () => {
   return (
     <>
-      <form className="flex flex-col lg:w-1/3 md:w-1/2">
-        <div className="flex m-3">
-          <label className="mt-1 mr-3">Name</label>
-          <input
-            type="text"
-            className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 
+      <form className="flex flex-row lg:w-1/3 md:w-1/2">
+        <div className="flex flex-col">
+          <div className="flex m-3">
+            <label className="mt-1 mr-3">Name</label>
+            <input
+              type="text"
+              className="w-3/5 bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 
           text-base outline-none text-gray-700 px-3 leading-8 transition-colors duration-200 ease-in-out mb-4"
-          />
-        </div>
-        <div className="flex m-3">
-          <label className="mt-1 mr-3">Phone</label>
-          <input
-            type="text"
-            className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 
+            />
+          </div>
+          <div className="flex m-3">
+            <label className="mt-1 mr-3">Phone</label>
+            <input
+              type="text"
+              className="w-3/5 bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 
           text-base outline-none text-gray-700 px-3 leading-8 transition-colors duration-200 ease-in-out mb-4"
-          />
-        </div>
-        <div className="flex m-3">
-          <label className="mt-1 mr-3">Amount (Rs)</label>
-          <input
-            type="text"
-            className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 
+            />
+          </div>
+          <div className="flex m-3">
+            <label className="mt-1 mr-3">Amount (Rs)</label>
+            <input
+              type="text"
+              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 
             text-base outline-none text-gray-700 px-3 leading-8 transition-colors duration-200 ease-in-out mb-4"
-          />
+            />
+            <div className="ml-4">
+              <button className="text-white bg-indigo-500 border-0 py-1 px-2 focus:outline-none hover:bg-indigo-600 rounded text-sm">
+                Submit
+              </button>
+            </div>
+          </div>
         </div>
-        <button className="text-white bg-indigo-500 border-0 py-1 px-2 focus:outline-none hover:bg-indigo-600 rounded text-sm">
-          Submit
-        </button>
       </form>
     </>
   );
